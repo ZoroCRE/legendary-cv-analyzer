75125d29b752902797bd14ce717e22239b2be1d9
Zoro_RCE <44169685+ZoroCRE@users.noreply.github.com>
2025-08-11 21:08:17 +0300

    Update supabaseService.ts

--- backend/src/services/supabaseService.ts
+++ backend/src/services/supabaseService.ts
@@ -1,7 +1,5 @@
-// backend/src/services/supabaseService.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_KEY!;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+  const supabaseUrl = process.env.SUPABASE_URL || '';
+  const supabaseKey = process.env.SUPABASE_KEY || '';
+  export const supabase = createClient(supabaseUrl, supabaseKey);
